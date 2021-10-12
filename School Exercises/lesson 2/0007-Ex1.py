8b152a76325295d697b4adbb5c7f6cace882951c
C1C0 <kpskola1002@gmail.com>
2021-10-12 10:13:28 +0200

    Exercise 2

--- School Exercises/lesson 2/0007-Ex1.py
+++ School Exercises/lesson 2/0007-Ex1.py
@@ -1,11 +1,20 @@
-#1 
+# 1
 
 a = 15 * 5
 print(a)
 
-#1.1
+# 1.1
 
 x = float(input("Put float: "))
 
 y = 3*(x**4) + 3*(x**2) - 10
 print("y =", y)
+
+# 2
+num = int(input("Enter a number: "))
+
+modulus = num % 2
+if modulus > 0:
+    print("You picked an odd number")
+else:
+    print("You picked an even number")
