47baa05505e6fbc7f37b5f222ec8eb6d77c56b83
C1C0 <kpskola1002@gmail.com>
2021-11-03 17:36:08 +0100

    Solution of game from course

--- Course Exercise/Module4-Tic-Tac-Toe.py
+++ Course Exercise/Module4-Tic-Tac-Toe.py
@@ -1,228 +1,97 @@
 from random import randrange
 
-userSymbol = "O"
-computerSymbol = "X"
 
-# we expect: Width == height
-board = (
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-)
-fieldsList = []
-
-try:
-    # get width and height and check them, as they should be similar
-    boardWidth = len(board)
-    boardHeight = len(board[0])
-except IndexError:
-    print("Board has nothing inside, exiting program...")
-    exit()
-except:
-    print("Unrecognized error occured.")
-    exit()
+def display_board(board):
+	print("+-------" * 3,"+", sep="")
+	for row in range(3):
+		print("|       " * 3,"|", sep="")
+		for col in range(3):
+			print("|   " + str(board[row][col]) + "   ", end="")
+		print("|")
+		print("|       " * 3,"|",sep="")
+		print("+-------" * 3,"+",sep="")
 
 
-# dimesions should be similar
-if boardWidth != boardHeight:
-    print("Board has different dimensions, exiting program...")
-    exit()
+def enter_move(board):
+	ok = False	# fake assumption - we need it to enter the loop
+	while not ok:
+		move = input("Enter your move: ") 
+		ok = len(move) == 1 and move >= '1' and move <= '9' # is user's input valid?
+		if not ok:
+			print("Bad move - repeat your input!") # no, it isn't - do the input again
+			continue
+		move = int(move) - 1 	# cell's number from 0 to 8
+		row = move // 3 	# cell's row
+		col = move % 3		# cell's column
+		sign = board[row][col]	# check the selected square
+		ok = sign not in ['O','X'] 
+		if not ok:	# it's occupied - to the input again
+			print("Field already occupied - repeat your input!")
+			continue
+	board[row][col] = 'O' 	# set '0' at the selected square
 
 
-def boardDivider(width):
-    """
-        if length 3 - then:\n
-        +-------+-------+-------+
-    """
-
-    boardPart = ""
-    for i in range(width):
-        boardPart += "+-------"
-
-        if i == width - 1:
-            boardPart += "+"
-
-    return boardPart
-
-def boardNoCharSpace(width):
-    """
-        if length 3 - then:\n
-        |sssssss|sssssss|sssssss|
-    """
-
-    boardPart = ""
-    for i in range(width):
-        boardPart += "|       "
-
-        if i == width - 1:
-            boardPart += "|"
-
-    return boardPart
-
-def boardCharSpace(row):
-    """
-        if length 3 - then:\n
-        |sss1sss|sss2sss|sss3sss| 
-    """
-
-    boardPart = ""
-    for i in range(len(row)):
-        boardPart += "|   {}   ".format(row[i])
-
-        if i == len(row) - 1:
-            boardPart += "|"
-
-    return boardPart
-
-def display_board(board, width):
-    '''
-        board - board with same dimmension - basically two dimensionall list \n
-        width - checked with of the board \n
-        height - checked height of the board \n
-    '''
-
-    for row in board:
-        print(boardDivider(width))
-        print(boardNoCharSpace(width))
-        print(boardCharSpace(row))
-        print(boardNoCharSpace(width))
-
-        # get last board divider - as border
-        if board.index(row) == width - 1:
-                print(boardDivider(width))
+def make_list_of_free_fields(board):
+	free = []	# the list is empty initially
+	for row in range(3): # iterate through rows
+		for col in range(3): # iterate through columns
+			if board[row][col] not in ['O','X']: # is the cell free?
+				free.append((row,col)) # yes, it is - append new tuple to the list
+	return free
 
 
-
-def enter_move(board, width, fieldsList):
-    """
-        Board - actual board status
-        width - width of board
-        height - height of board
-    """
-
-    maxValue = width * width
-
-    try:
-        nextMove = int(input("Please, enter your next move (number from: {} - {}): ".format(1, maxValue)))
-
-        # If out of range
-        if nextMove < 0 or nextMove > maxValue:
-            print("You provided wrong value, please, try again.")
-            return False
-
-        nextMove -= 1
-
-        row, i = fieldsList[nextMove]
-
-        # check if not taken
-        if type(board[row][i]) != int:
-            print("This place is already taken, please, provide different number")
-            return False
-
-        board[row][i] = "O"
-
-        return True
-    except ValueError:
-        print("You provided wrong value, please, try again.")
-        return False
+def victory_for(board,sgn):
+	if sgn == "X":	# are we looking for X?
+		who = 'me'	# yes - it's computer's side
+	elif sgn == "O": # ... or for O?
+		who = 'you'	# yes - it's our side
+	else:
+		who = None	# we should not fall here!
+	cross1 = cross2 = True  # for diagonals
+	for rc in range(3):
+		if board[rc][0] == sgn and board[rc][1] == sgn and board[rc][2] == sgn:	# check row rc
+			return who
+		if board[0][rc] == sgn and board[1][rc] == sgn and board[2][rc] == sgn: # check column rc
+			return who
+		if board[rc][rc] != sgn: # check 1st diagonal
+			cross1 = False
+		if board[2 - rc][2 - rc] != sgn: # check 2nd diagonal
+			cross2 = False
+	if cross1 or cross2:
+		return who
+	return None
 
 
-def make_list_of_free_fields(width):
-    """
-    Makes list of tuples with free places
-    """
-
-    availablePositions = []
-
-    for row in range(width):
-        for i in range(width):
-            availablePositions.append((row, i))
-
-    return availablePositions
+def draw_move(board):
+	free = make_list_of_free_fields(board) # make a list of free fields
+	cnt = len(free)
+	if cnt > 0:	# if the list is not empty, choose a place for 'X' and set it
+		this = randrange(cnt)
+		row, col = free[this]
+		board[row][col] = 'X'
 
 
+board = [ [3 * j + i + 1 for i in range(3)] for j in range(3) ] # make an empty board
+board[1][1] = 'X' # set first 'X' in the middle
+free = make_list_of_free_fields(board)
+human_turn = True # which turn is it now?
+while len(free):
+	display_board(board)
+	if human_turn:
+		enter_move(board)
+		victor = victory_for(board,'O')
+	else:	
+		draw_move(board)
+		victor = victory_for(board,'X')
+	if victor != None:
+		break
+	human_turn = not human_turn		
+	free = make_list_of_free_fields(board)
 
-def victory_for(board, width, sign):
-    """
-    The function analyzes the board's status in order to check if 
-    the player using 'O's or 'X's has won the game
-    """
-
-    cross1 = cross2 = True
-
-    for row in range(width):
-        # check row
-        if board[row][0] == sign and board[row][1] == sign and board[row][2] == sign:
-            return True
-
-        # check column
-        if board[0][row] == sign and board[1][row] == sign and board[2][row] == sign:
-            return True
-
-        # check diagonal from 1 to 9
-        if board[row][row] != sign:
-            cross1 = False
-
-        # check diagonal from 3 to 7
-        if board[2 - row][2 - row] != sign:
-            cross2 = False
-
-    print("Crosses:", cross1 or cross2)
-
-    return cross1 or cross2    
-
-
-def draw_move(board, width, fieldsList):
-    """
-    The function draws the computer's move and updates the board.
-    """
-
-    maxValue = width * width
-    computerMove = randrange(maxValue)
-
-    row, i = fieldsList[computerMove]
-
-    computerMove -= 1
-
-    # check if not taken
-    if type(board[row][i]) != int:
-        return False
-
-    board[row][i] = "X"
-
-    return True
-
-
-fieldsList = make_list_of_free_fields(boardWidth)
-winner = ""
-numberOfMoves = 0
-while True:
-    if numberOfMoves > boardWidth * boardWidth:
-        break
-
-    # Computer Move
-    while not draw_move(board, boardWidth, fieldsList):
-        print(end="")
-    
-    if victory_for(board, boardWidth, computerSymbol):
-        winner = "Computer"
-        break
-
-    # display
-    display_board(board, boardWidth)
-
-    # Players move
-    while not enter_move(board, boardWidth, fieldsList):
-        print()
-
-    if victory_for(board, boardWidth, userSymbol):
-        winner = "Player"
-        break
-
-    numberOfMoves += 2
-
-display_board(board, boardWidth)
-if winner == "":
-    print("Tie")
+display_board(board)
+if victor == 'you':
+	print("You won!")
+elif victor == 'me':
+	print("I won")
 else:
-    print("The winner is", winner)
+	print("Tie!")
